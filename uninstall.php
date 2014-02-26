02daab4e925a19138c8a9c48f3eb382e776f4e9f
Julien Liabeuf <julien@liabeuf.fr>
2014-02-26 16:43:54 +0700

    Delete all plugin data on uninstall

--- uninstall.php
+++ uninstall.php
@@ -14,5 +14,49 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-/* Delete the dismissed notifications list */
-delete_option( '_rn_dismissed' );
+/* Delete channels and post types */
+delete_custom_terms( 'rn-channel' );
+delete_custom_terms( 'rn-pt' );
+
+/**
+ * Delete all the notifications saved in DB
+ */
+$args = array(
+	'numberposts'			 => -1,
+	'post_type' 			 => 'notification',
+	'update_post_term_cache' => false,
+	'update_post_meta_cache' => false,
+	'cache_results' 		 => false
+);
+
+$posts = new WP_Query( $args );
+
+if( isset( $posts->posts ) && is_array( $posts->posts ) ) {
+
+   foreach( $posts->posts as $post )
+       wp_delete_post( $post->ID, true );
+
+}
+
+/**
+ * Detete all terms of a taxonomy
+ *
+ * @link http://wordpress.stackexchange.com/questions/119229/how-to-delete-custom-taxonomy-terms-in-plugins-uninstall-php
+ */
+function delete_custom_terms( $taxonomy ) {
+
+	global $wpdb;
+
+	$query = 'SELECT t.name, t.term_id
+	FROM ' . $wpdb->terms . ' AS t
+	INNER JOIN ' . $wpdb->term_taxonomy . ' AS tt
+	ON t.term_id = tt.term_id
+	WHERE tt.taxonomy = "' . $taxonomy . '"';
+
+	$terms = $wpdb->get_results($query);
+
+	foreach( $terms as $term ) {
+		wp_delete_term( $term->term_id, $taxonomy );
+	}
+
+}
